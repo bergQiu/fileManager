2f1b045284b45b475986b7c577c4b384b6b9e953
berg <qiuzonglin@adasplus.com>
2021-04-14 12:19:43 +0800

    修复已知bug并完善部分逻辑

--- js/file_manager.js
+++ js/file_manager.js
@@ -18,7 +18,7 @@
             folders: {},
             defaultFolder: '/assets',
             checkedFolders: [], 
-            showType: 'icon' // 'icon', 'list'
+            showType: 'list' // 'icon', 'list'
         };
         this.options = $.extend(true, default_, options)
     }
@@ -50,12 +50,16 @@
 
             _this.$target.find('.show-type-change').click(function(){
                 let type = $(this).find('i').hasClass('fa-th') ? 'icon': 'list';
+
                 if(type == 'icon'){
                     $(this).find('i').removeClass('fa-th').addClass('fa-list');
+                    type = 'list'
                 }else if(type == 'list'){
-                    $(this).find('i').removeClass('fa-list').addClass('fa-th')
+                    $(this).find('i').removeClass('fa-list').addClass('fa-th');
+                    type = 'icon'
                 }
                 _this.showType = type;
+                _this.renderTargetBody();
             })
 
             _this.$target.find('.last-path').click(function(){
@@ -66,15 +70,24 @@
                 _this.render();
             })
 
+            _this.$target.find('.home-path').click(function(){
+                _this.presentFolder = '/';
+                _this.render();
+            })
+
+            _this.$target.find('.close').click(function(){
+                _this.$target.slideToggle();
+            })
+
             $(document).on('click', `#${_this.uid}-target-div .path-child`, function(){
                 let path = $(this).data('path');
                 _this.presentFolder = path
                 _this.render();
             })
 
-            console.log($(`#${_this.uid}-target-div .folder-icon-item`).not('input'));
-            $(document).on('dblclick' ,$(`#${_this.uid}-target-div .folder-icon-item`).not('input'), function(){
+            $(document).on('dblclick', `#${_this.uid}-target-div .folder-item`, function(e){
                 let path = $(this).data('path');
+                
                 if(_this.folders[path]){
                     _this.presentFolder = path
                     _this.render();
@@ -83,10 +96,23 @@
                 }
             })
 
-            $(document).on('change' ,`#${_this.uid}-target-div input[name=checked-folder]`, function(evt){
+            $(document).on('change' ,`#${_this.uid}-target-div input[name=checked-folder]`, function(){
                 let path = $(this).data('path');
                 let action = $(this).is(':checked');
                 _this.checkedPathChange(path, action);
+            })
+
+            $(document).on('dblclick', `#${_this.uid}-target-div input[name=checked-folder]`, function(evt){
+                evt.stopPropagation();
+                evt.preventDefault();
+            })
+
+            $(document).on('click', `#${_this.uid}-target-div .target-footer .checked-item-del`, function(){
+                let path = $(this).data('path');
+                let new_ = _this.checkedFolders.filter(f => f != path);
+                _this.checkedFolders = new_;
+                _this.render();
+                _this.renderCheckedFolders();
             })
         },
         openManagerLayer: function(){
@@ -112,13 +138,13 @@
                                 <hr class='my-hr'>
                                 <div class='layui-row'><span>路径: </span><span class='now-path'></span></div><hr class='my-hr'>
                             </div>
-                            <div id='${this.uid}-target-body' class='layui-row target-body'>
+                            <div id='${this.uid}-target-body' class='layui-row target-body my-scrollbar'>
                                 <!-- <div class='layui-col-md8 folder-show'></div>
                                 <div class='layui-col-md4 folder-info'></div> -->
                             </div><hr class='my-hr'>
                             <div id='${this.uid}-target-footer' class='layui-row target-footer'>
                                 <div class='layui-col-md1 checked-title'>已选路径</div>
-                                <div class='layui-col-md11 checked-path'></div>
+                                <div class='layui-col-md11 checked-path my-scrollbar'></div>
                             </div>
                         </div>`
 
@@ -167,14 +193,17 @@
             // return `<div>${body}</div>`
         },
         foldersIconEx: function(folder){
-            return `<div class='folder-icon-item' data-path='${folder.path}'>
+            return `<div class='folder-item folder-icon-item' data-path='${folder.path}'>
                         <div class='check'><input type='checkbox' data-path='${folder.path}' name='checked-folder'></div>
                         <div title='${folder.path}'><i class='fa fa-folder-open-o fa-3x'></i></div>
                         <span title='${folder.path.split('/').pop()}'>${folder.path.split('/').pop()}</span>
                     </div>`        
         },
         foldersListEx: function(folder){
-            return
+            return `<div class='folder-item folder-list-item' data-path='${folder.path}'>
+                        <div class='check'><input type='checkbox' data-path='${folder.path}' name='checked-folder'></div>
+                        <div><span title='${folder.path}'>${folder.path.split('/').pop()}</span></div>
+                    </div>`
         },
         checkedPathChange: function(path, action){
             // 改变当前选中的事件
@@ -185,37 +214,20 @@
                 // 没选中到选中
                 this.checkWholeCheck(path);
                 flag = true;
-                // if(allCheck){
-                //     if(!this.checkedFolders.includes(path)){
-                //         this.checkedFolders.push(path);
-                //         flag = true;
-                //     }
-                // }else{
-                //     // this.checkWholeCheck(path);
-                //     //当前路径都选中
-                //     // let f = $.extend(true, [], path.split('/'))
-                //     // f.pop();
-                //     // let f_path = f.join('/')
-                //     // let f_path_children = this.folders[f_path].map(f => {return f.path});
-                //     // let checked_folders = this.checkedFolders;
-                //     // let new_ = checked_folders.filter(c => {
-                //     //     return !f_path_children.includes(c)
-                //     // })
-
-                //     // new_.push(f_path)
-                //     // this.checkedFolders = new_;
-                //     // flag = true;
-                // }
             }else{
                 // 选中到不选中
                 if(allCheck != 1){
+                    console.log(4);
                     if(this.checkedFolders.includes(path)){
                         let new_ = this.checkedFolders.filter(f => {return f != path});
                         this.checkedFolders = new_;
                         flag = true;
                     }
                 }else{
-                    // 当前路劲全选中然后删除其中的一个
+                    // 当前路径全选中然后删除其中的一个
+                    let ffpath = this.checkedFolders.filter(f => path.indexOf(f) == 0)[0];
+                    this.checkNotWholeCheck(ffpath, path);
+                    flag = true;
                 }
             }
 
@@ -227,7 +239,7 @@
 
             if(checked){
                 checked.map((c, i) => {
-                    body += `<div class='layui-row folder-checked-item'><div class='layui-col-md10'>${i + 1}、${c}</div><div class='layui-col-md1 checked-item-del'><i class='fa fa-trash'></i></div></div>`
+                    body += `<div class='layui-row folder-checked-item'><div class='layui-col-md10'>${i + 1}、${c}</div><div class='layui-col-md1 checked-item-del' data-path='${c}'><i class='fa fa-trash' title='删除'></i></div></div>`
                 })
             }
 
@@ -261,22 +273,47 @@
         },
         checkWholeCheck: function(path){
             // 检查全选
+
             let f = $.extend(true, [], path.split('/'))
             f.pop();
-            let f_path = f.join('/'); // 父层
-            let f_path_children = this.folders[f_path].map(f => {return f.path}); // 父的子路径
+            let f_path = f.length == 1 ? '/': f.join('/'); // 父层
+            let f_path_children = this.folders[f_path] .map(f => {return f.path}); // 父的子路径
             let checked_folders = this.checkedFolders.concat([path]); // 已选中
             let whole_check = f_path_children.every(f => checked_folders.includes(f));
-            
             if(whole_check){
+                console.log(1);
                 let new_ = this.checkedFolders.filter(c => {
                         return !f_path_children.includes(c)
                     })
 
-                new_.push(f_path)
                 this.checkedFolders = new_;
                 this.checkWholeCheck(f_path)
             }else{
+                this.checkedFolders.push(path)
+            }
+        },
+        checkNotWholeCheck: function(ffpath, path){
+            // 检查不全选
+            let checked = this.checkedFolders.concat([]);
+            let ffpath_child = this.folders[ffpath].map(f => f.path);
+            
+            let a = checked.filter( c => {return  c != ffpath})
+            a.push.apply(a, ffpath_child)
+            
+            let ffpath_ = ''
+            a.some(f => {
+                if(path.indexOf(f) == 0){
+                    ffpath_ = f;
+                    return true
+                }
+            })
+
+            this.checkedFolders = a;
+
+            if(ffpath_ != path ){
+                this.checkNotWholeCheck(ffpath_, path)
+            }else{
+                this.checkedFolders = a.filter(a_ => a_ != path)
                 return false
             }
         }
